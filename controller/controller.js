078b12eb9cc7d6f0f43338bd6c1abf657279d585
manglesh23 <mangleshyadav2@gmail.com>
2024-05-28 09:26:21 +0530

    javaScript

--- controller/controller.js
+++ controller/controller.js
@@ -10,4 +10,37 @@ const home=async(req,res)=>{
     }
 }
 
+function squre(num){ // function declaration  
+  return num*num;
+}
+
+const squre1= function(num){   //function expression when function is stored in a veriable
+    return num*num
+}                             // in this function doesn't have name--it's called anynonums function
+console.log(squre1(6));
+
+//first class function
+function mulit(num1,num){     // fucntion can be treated like a variable, can be passed to another function
+    return num1*num  
+}
+
+function display(fn){
+    console.log("First class function:-",fn(7,9));
+}
+
+display(mulit);
+
+
+//IIFE--->imidiealty invoked function expression
+
+(function sum(num,value){
+    console.log(num+value);   //function was called from sum---then the function declaration closed in open paraenthiess
+})(7,3)
+
+// sum(7,1);
+
+let numbers=[1,2,3,4,5,6];
+let multiply=numbers.map(item=>item*2);
+console.log( multiply)
+
 module.exports={home};
