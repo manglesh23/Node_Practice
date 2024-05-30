50d6952e14bcddaab6bf1c8ceef6e074168b057b
manglesh23 <mangleshyadav2@gmail.com>
2024-05-30 11:20:13 +0530

    data structure

--- controller/dataStructure.js
+++ controller/dataStructure.js
@@ -1,8 +1,36 @@
 console.log("data Structure jjbjfbs");
 const dataStructure=(req,res)=>{
     console.log("main manish hu");
-    res.status(200).json({message:"Data Strucre sjdfsdfjh"});
+    res.status(200).json({message:"Data Structure"});
 
 }
+
+let num=101;
+let name="malylam";
+const checkpailanDrpmee=(num)=>{
+    console.log(typeof num);
+    if(typeof(num)==='number'){
+    return num===+num.toString().split("").reverse().join("");
+    }else{
+        return num===num.split("").reverse().join("");
+    }
+}
+const value=checkpailanDrpmee(name);
+console.log("value:-",value);
+
+const fib=(n)=>{
+    if(n<=1)
+        return n;
+    
+    return fib(n-1)+fib(n-2);
+}
+console.log(fib(3));
+
+function anagram(s,t){
+    s =s.split("").sort().join("");
+    t=t.split("").sort().join("");
+    return s===t
+}
+console.log("Anagram Check:-",anagram('anme','name'))
 
 module.exports={dataStructure}
