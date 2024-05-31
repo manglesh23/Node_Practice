0d35e7d81a1f1948e508a09fa77e4b77825a2771
manglesh23 <mangleshyadav2@gmail.com>
2024-05-31 14:46:10 +0530

    sting

--- controller/stringdataStructure.js
+++ controller/stringdataStructure.js
@@ -1,4 +1,10 @@
 const StringDataStructure=async(req,res)=>{
     res.status(200).json({message:"String Data Structure File"});
 }
+
 module.exports={StringDataStructure};
+
+const str="manglesh yadav 123231";
+console.log(str);
+console.log(str.replaceAll("a","z"));
+//concat --to add string with another string
