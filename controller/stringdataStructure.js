8a438b7cb16a72d59e92033e4296ab487f306d68
manglesh23 <mangleshyadav2@gmail.com>
2024-05-31 16:29:51 +0530

    string

--- controller/stringdataStructure.js
+++ controller/stringdataStructure.js
@@ -8,3 +8,25 @@ const str="manglesh yadav 123231";
 console.log(str);
 console.log(str.replaceAll("a","z"));
 //concat --to add string with another string
+console.log(str.concat(" ","shaktiman"))  ;
+
+let anagramstring="Hello World With You";
+let anotherstring="World Hello You With";
+
+function checkanagram(str1,str2){
+    console.log(str1,str2);
+    if(str1.length!=str2.length)
+        return false
+
+    str1=str1.split(" ");
+    console.log(str1);
+
+    for(let v of str1){
+        console.log(v)
+        if(!str2.includes(v));
+        return false
+    }
+    return true
+}
+
+console.log("Check Anagram:-",checkanagram(anagramstring,anotherstring));
